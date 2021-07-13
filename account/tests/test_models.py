7b4b16d25ea8791953da851b64574ac1e7e1ae1a
saviganga <akatugbasaviour@gmail.com>
2021-07-13 08:19:03 -0700

    added tests for models

---
+++ account/tests/test_models.py
@@ -0,0 +1,45 @@
+from django.test import TestCase
+from account.models import MyUser, Team, Player, Fan
+
+class MyUserModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.test_user1 = MyUser.objects.create(
+            email='testuser1@testuser.com', 
+            name='testuser1',
+            is_team=True
+        )
+
+        cls.test_user2 = MyUser.objects.create(
+            email='testuser2@testuser.com',
+            name='testuser2',
+            is_player=True
+        )
+
+        cls.test_user3 = MyUser.objects.create(
+            email='testuser3@testuser.com',
+            name='testuser3',
+            is_fan=True
+        )
+
+    def test_user1_type(self):
+        self.assertEqual(self.test_user1.is_fan, False)
+        self.assertEqual(self.test_user1.is_player, False)
+        self.assertEqual(self.test_user1.is_team, True)
+        self.assertEqual(self.test_user2.is_team, False)
+        self.assertEqual(self.test_user3.is_team, False)
+        self.assertEqual(self.test_user3.is_player, False)
+
+    def test_user2_type(self):
+        self.assertEqual(self.test_user2.is_fan, False)
+        self.assertEqual(self.test_user2.is_player, True)
+        self.assertEqual(self.test_user2.is_team, False)
+
+    def test_user3_type(self):    
+        self.assertEqual(self.test_user3.is_fan, True)
+        self.assertEqual(self.test_user1.is_player, False)
+        self.assertEqual(self.test_user2.is_team, False)
+
+
+# Create your tests here.
